9b861c002431676c29770997ba3f7da22a0235a1
AEMaximinos <diogofssilva7@gmail.com>
2021-02-16 15:08:30 +0000

    Preparar deployment - HEROKU

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, request
 from user import User
 
 app = Flask(__name__)
-db = User()
+usr = User()
 
 
 @app.route('/tabela')
 def tabela():
-    dados = db.lista()
+    dados = usr.lista()
     return render_template('tabela.html', tabela=dados, max=len(dados))
 
 @app.route('/registo', methods=['GET', 'POST'])
@@ -18,12 +18,12 @@ def route():
         v2 = request.form['email']
         v3 = request.form['passe']
         v4 = request.form['cpasse']
-        if db.existe(v1):
+        if usr.existe(v1):
             erro = 'O Utilizador já existe.'
         elif v3 != v4:
             erro = 'A palavra passe não coincide.'
         else:
-            db.gravar(v1, v2, v3)
+            usr.gravar(v1, v2, v3)
     return render_template('registo.html', erro=erro)
 
 
@@ -38,9 +38,9 @@ def login():
     if request.method == 'POST':
         v1 = request.form['utilizador']
         v2 = request.form['passe']
-        if not db.existe(v1):
+        if not usr.existe(v1):
             erro = 'O Utilizador não existe.'
-        elif not db.log(v1, v2):
+        elif not usr.log(v1, v2):
             erro = 'A palavra passe está errada.'
         else:
             erro = 'Bem-Vindo.'
@@ -53,12 +53,12 @@ def apagar():
     if request.method == 'POST':
         v1 = request.form['utilizador']
         v2 = request.form['passe']
-        if not db.existe(v1):
+        if not usr.existe(v1):
             erro = 'O Utilizador não existe.'
-        elif not db.log(v1, v2):
+        elif not usr.log(v1, v2):
             erro = 'A palavra passe está errada.'
         else:
-            db.apaga(v1)
+            usr.apaga(v1)
             erro = 'Conta Eliminada com Sucesso.'
     return render_template('apagar.html', erro=erro)
 
@@ -71,14 +71,14 @@ def newpasse():
         v0 = request.form['apasse']
         v2 = request.form['passe']
         v3 = request.form['cpasse']
-        if not db.existe(v1):
+        if not usr.existe(v1):
             erro = 'O Utilizador não existe.'
-        elif not db.log(v1, v0):
+        elif not usr.log(v1, v0):
             erro = 'A palavra passe está errada.'
         elif v2 != v3:
             erro = 'A palavra passe não coincide.'
         else:
-            db.alterar(v1, v2)
+            usr.alterar(v1, v2)
     return render_template('newpasse.html', erro=erro)
 
 
--- user.py
+++ user.py
@@ -1,5 +1,5 @@
 import psycopg2
-import db
+
 
 class User:
     db = 'db/Utilizadores.xlsx'
@@ -9,7 +9,7 @@ class User:
 
     def reset(self):
         self.id = None
-        self.cliente = ''
+        self.login = ''
         self.email = ''
         self.password = ''
         self.nif = ''
@@ -21,44 +21,45 @@ class User:
         mydb = Database()
         return psycopg2.connect(host=mydb.Host, database=mydb.Database, user=mydb.User, password=mydb.Password, sslmode='require')
 
-    def gravar(self, v1, v2, v3):
+    def gravar(self, login, email, password):
         ficheiro = self.herokudb()
         db = ficheiro.cursor()
-        db.execute("CREATE TABLE IF NOT EXISTS usr (nome text,email text, passe text)")
-        db.execute("INSERT INTO usr VALUES (%s, %s, %s)", (v1, v2, self.code(v3)))
+        #db.execute("drop table usr")
+        db.execute("CREATE TABLE IF NOT EXISTS usr (id serial primary key, login text, email text, password text, nif text, nome text, morada char (60))")
+        db.execute("INSERT INTO usr VALUES (DEFAULT, %s, %s, %s)", (login, email, self.code(password),))
         ficheiro.commit()
         ficheiro.close()
 
-    def existe(self, v1):
+    def existe(self, login):
         try:
             ficheiro = self.herokudb()
             db = ficheiro.cursor()
-            db.execute("SELECT * FROM usr WHERE nome = %s", (v1,))
+            db.execute("SELECT * FROM usr WHERE login = %s", (login,))
             valor = db.fetchone()
             ficheiro.close()
         except:
             valor = None
         return valor
 
-    def log(self, v1, v2):
+    def log(self, login, password):
         ficheiro = self.herokudb()
         db = ficheiro.cursor()
-        db.execute("SELECT * FROM usr WHERE nome = %s and passe = %s", (v1, self.code(v2),))
+        db.execute("SELECT * FROM usr WHERE login = %s and password = %s", (login, self.code(password),))
         valor = db.fetchone()
         ficheiro.close()
         return valor
 
-    def alterar(self, v1, v2):
+    def alterar(self, login, password):
         ficheiro = self.herokudb()
         db = ficheiro.cursor()
-        db.execute("UPDATE usr SET passe = %s WHERE nome = %s", (self.code(v2), v1))
+        db.execute("UPDATE usr SET password = %s WHERE login = %s", (self.code(password), login))
         ficheiro.commit()
         ficheiro.close()
 
-    def apaga(self, v1):
+    def apaga(self, login):
         ficheiro = self.herokudb()
         db = ficheiro.cursor()
-        db.execute("DELETE FROM usr WHERE nome = %s", (v1,))
+        db.execute("DELETE FROM usr WHERE login = %s", (login,))
         ficheiro.commit()
         ficheiro.close()
 
@@ -66,7 +67,7 @@ class User:
         try:
             ficheiro = self.herokudb()
             db = ficheiro.cursor()
-            db.execute("select * from usr ORDER BY nome")
+            db.execute("select * from usr")
             valor = db.fetchall()
             ficheiro.close()
         except:
